7ee68558d5d580264c3b0078d3eda893895e88f2
Ahn Seongjin <jam45@naver.com>
2019-01-04 20:26:37 +0900

    clean-up with comments

--- src/test/solver/tridiag.cu
+++ src/test/solver/tridiag.cu
@@ -51,16 +51,12 @@ int main(int argc, char *argv[]) {
   
   //// Initialize status variables
   cusparseStatus_t cusparse_status = CUSPARSE_STATUS_SUCCESS;
-//  cudaError_t cu_status = cudaSuccess;	
 
   //// Initialize event variables
   cudaEvent_t start, stop;
   cu_err_check( cudaEventCreate(&start) );
-//  cu_status = cudaEventCreate(&start);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   cu_err_check( cudaEventCreate(&stop) );
-//  cu_status = cudaEventCreate(&stop);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
+  float elapsed_time_forward, elapsed_time_backward;
 
 
   long i;
@@ -76,7 +72,6 @@ int main(int argc, char *argv[]) {
     *ph[num_of_aug_arrays], *h_x_aug,
     *pd[num_of_aug_arrays];
   for (i=0; i<num_of_aug_arrays; ++i) {
-//    if (i==i_x) { continue; }
     tmp = (m_t *) malloc(size_of_arr);
     if (tmp != NULL) { ph[i] = tmp; }
     else { fprintf(stderr,"[ERROR] during `malloc()`"); return -1; }
@@ -98,16 +93,6 @@ int main(int argc, char *argv[]) {
   h_ld[0] = 0.0; h_ud[N-1] = 0.0;
   h_x[-1] = 0.0; h_x[N] = 0.0;
 
-  //// (will be replaced by gpu code) Forward multiplication
-//  m_t *buf = (m_t *) malloc(N*size_of_m_t);
-//  buf[0] = h_d[0] * h_x[0] + h_ud[0] * h_x[1];
-//  for (i=1; i<N-1; ++i) {
-//    buf[i] = h_ld[i] * h_x[i-1] + h_d[i] * h_x[i] + h_ud[i] * h_x[i+1];
-//  }
-//  buf[N-1] = h_ld[N-1] * h_x[N-2] + h_d[N-1] * h_x[N-1];
-//  memcpy(h_x, buf, N*size_of_m_t);
-//  free(buf);
-
   //// Print arrays before the calculation
 //  std::cout << "h_x (before): ";
 //  for (i=0; i<N; ++i) {
@@ -119,38 +104,30 @@ int main(int argc, char *argv[]) {
   
   //// Allocate device memory and copy contents from the host
   for (i=0; i<num_of_aug_arrays; ++i) {
-//    if (i==i_x) { continue; }
     cu_err_check( cudaMalloc(&pd[i], N*size_of_m_t) );
-//    cu_status = cudaMalloc(&pd[i], N*size_of_m_t);
-//    if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
     cu_err_check ( cudaMemcpy(pd[i], ph[i], N*size_of_m_t, cudaMemcpyHostToDevice) );
-//    cu_status = cudaMemcpy(pd[i], ph[i], N*size_of_m_t, cudaMemcpyHostToDevice);
-//    if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   }
   m_t *d_x_aug = NULL;
   cu_err_check( cudaMalloc(&d_x_aug, size_of_augmented_arr) );
-//  cu_status = cudaMalloc(&d_x_aug, size_of_augmented_arr);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   cu_err_check( cudaMemcpy(d_x_aug, h_x_aug, size_of_augmented_arr, cudaMemcpyHostToDevice) );
-//  cu_status = cudaMemcpy(d_x_aug, h_x_aug, size_of_augmented_arr, cudaMemcpyHostToDevice);
 
 //  pd[i_x] = d_x_aug + 1;
 
   m_t *d_b = NULL;
   cu_err_check( cudaMalloc(&d_b, size_of_arr) );
-//  cu_status = cudaMalloc(&d_b, size_of_arr);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
+
 
 
   //// Run forward tridiagonal multiplication on device
+  // Configuration
   int num_of_thread_per_block = 128;
   int num_of_blocks_max = 32;
   int num_of_blocks = MIN((N+num_of_thread_per_block-1)/num_of_thread_per_block, num_of_blocks_max);
+  // Execution
   cudaEventRecord(start, 0);
   tridiag_forward<<<num_of_blocks, num_of_thread_per_block>>>(N, pd[i_ld], pd[i_d], pd[i_ud], d_x_aug, d_b);
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
-  float elapsed_time_forward, elapsed_time_backward;
   cudaEventElapsedTime(&elapsed_time_forward, start, stop);
   // logging
   fprintf(stdout, "[ LOG ] Launched kernel `tridiag_forward` with `%d` blocks with `%d` threads each\n",
@@ -160,8 +137,6 @@ int main(int argc, char *argv[]) {
   // copy the intermediate data
   m_t *h_b = (m_t *) malloc(size_of_arr);
   cu_err_check( cudaMemcpy(h_b, d_b, size_of_arr, cudaMemcpyDeviceToHost) );
-//  cu_status = cudaMemcpy(h_b, d_b, size_of_arr, cudaMemcpyDeviceToHost);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   free(h_b);
   // print
 //  std::cout << "h_b (between): ";
@@ -178,18 +153,12 @@ int main(int argc, char *argv[]) {
   cudaEventRecord(stop, 0);
   cudaEventSynchronize(stop);
   cudaEventElapsedTime(&elapsed_time_backward, start, stop);
-//  cusparse_status = cusparseDgtsv(handle, N, 1, pd[i_ld], pd[i_d], pd[i_ud], pd[i_x], N);
 
   //// Copy data from device to host
   for (i=0; i<num_of_aug_arrays; ++i) {
-//    if (i==i_x) { continue; }
     cu_err_check( cudaMemcpy(ph[i], pd[i], size_of_arr, cudaMemcpyDeviceToHost) );
-//    cu_status = cudaMemcpy(ph[i], pd[i], size_of_arr, cudaMemcpyDeviceToHost);
-//    if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   }
   cu_err_check( cudaMemcpy(h_x, d_b, size_of_arr, cudaMemcpyDeviceToHost) );
-//  cu_status = cudaMemcpy(h_x, d_b, size_of_arr, cudaMemcpyDeviceToHost);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   
 
   //// Print result
@@ -205,15 +174,10 @@ int main(int argc, char *argv[]) {
 
   //// Destroy events
   cu_err_check( cudaEventDestroy(start) );
-//  cu_status = cudaEventDestroy(start);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   cu_err_check( cudaEventDestroy(stop) );
-//  cu_status = cudaEventDestroy(stop);
-//  if (cu_status != cudaSuccess) { return cu_error_msg(cu_status); }
   
   //// Free allocated memory
   for (i=0; i<num_of_aug_arrays; ++i) {
-//    if (i==i_x) { continue; }
     free(ph[i]);
     cudaFree(pd[i]);
   }
